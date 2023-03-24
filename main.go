51a9284ea8d0aac980ed35e0efb0bb80ec06d0f8
kingsleyObot <kingsley.obot@sastrify.com>
2023-03-24 08:44:45 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/kingsman20/ecommerce-go-app/controllers"
+	"github.com/kingsman20/ecommerce-go-app/database"
+	middleware "github.com/kingsman20/ecommerce-go-app/middlewares"
+	"github.com/kingsman20/ecommerce-go-app/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run(":" + port))
+}
